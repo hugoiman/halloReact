6f393025d90f8bb53daef0738db571164227cd3d
hugoiman <hugogiman17@gmail.com>
2018-12-16 06:53:17 +0700

    Handling Events pada Class Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,12 @@ class Header extends Component{
       /*nama state = daftar*/ daftar : "Daftar Makanan Nusantara",
       dataList : this.props.list
     };
+    //  wajib menuliskan jika terdapat hadling event berasal dari state/props
+    this.handlePesan = this.handlePesan.bind(this);
+  }
+
+  handlePesan(){
+    alert(this.state.daftar);
   }
 
   render(){
@@ -21,6 +27,9 @@ class Header extends Component{
         <h2>Makanan Khas Indonesia</h2>
         <p>{this.state.daftar}</p>
         <p>{this.state.dataList}</p>
+        <a href="/" onClick={this.handlePesan}>
+          Halaman Header
+          </a>
       </div>
     )
   }
