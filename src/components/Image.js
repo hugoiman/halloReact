774ef41223131a8530f4b8ffdd2cfef59ca795df
hugoiman <hugogiman17@gmail.com>
2018-12-16 06:15:41 +0700

    Props pada Class Component

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,9 +4,9 @@ class Image extends Component {
   render() {
     return (
       <img
-        src="https://cdns.klimg.com/merdeka.com/i/w/news/2018/04/06/960698/670x335/hindari-konsumsi-7-makanan-ini-saat-kamu-sedang-mengalami-isk.jpg"
-        alt="Ayam Goreng"
-        width="500"
+        src={this.props.linkgambar}
+        alt="Gambar Makanan"
+        width={this.props.lebar}
       />
     );
   };
