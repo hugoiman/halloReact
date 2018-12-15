2a5c7da52e96d7b6a326db86d7e178b85ea76fd5
hugoiman <hugogiman17@gmail.com>
2018-12-16 05:35:26 +0700

    Penulisan Function Component

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
+//  Penulisan disarankan menggunakan cara ini
 const Footer = () => {
+  var nama = "Ogen"
   return (
     <div>
-      <h4>Halaman Footer</h4>
+      <h4>Halaman Footer {nama}</h4>
       <p>@2018</p>
     </div>
   );
 };
+
+//  Penulisan Function lain
+// function Footer () {
+//   return (
+//     <div>
+//       <h4>Halaman Footer</h4>
+//       <p>@2018</p>
+//     </div>
+//   );
+// };
 
 export default Footer;
