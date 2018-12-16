74bae446a7564c1f4213ee7c7e699ac49582599e
hugoiman <hugogiman17@gmail.com>
2018-12-16 07:28:55 +0700

    Multiple State

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,19 +5,24 @@ class Main extends Component {
   constructor(props){
     super(props);
     this.state = {
-      title: "Menu Makanan"
+      title: "Menu Makanan",
+      title2: "Menu Minuman"
     };
     this.rubahData = this.rubahData.bind(this);
   }
 
-  // Function untuk mengubah state
+  // Function untuk mengubah state: 2 cara
   // rubahData(){
   //   this.setState({title: "Pilih Makanan"});
   // }
 
+  //  Disarankan menggunakan ini
   rubahData(){
-    this.setState((state, props) => {
-      return { title : "Pilih Menu Makanan" };
+    this.setState((state, props) => { //  (state, props) digunakan jika value yang diambil dari state/props
+      return {
+        title : "Pilih Menu Makanan",
+        title2 : this.state.title
+      };
     });
   }
 
@@ -25,6 +30,7 @@ class Main extends Component {
     return (
       <div>
         <h3>{this.state.title}</h3>
+        <h2>{this.state.title2}</h2>
         <button onClick={this.rubahData}>
           Rubah data
         </button>
